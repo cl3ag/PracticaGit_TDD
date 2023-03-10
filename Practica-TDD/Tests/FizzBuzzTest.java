93399d5b0040a69fe901e570d1de3b2dd5f86be6
cl3ar <sergio.scr43@gmail.com>
2023-03-10 19:30:41 +0100

    Third Commit (Commit con el 7 el 8 el 9 y el 10)

--- Practica-TDD/Tests/FizzBuzzTest.java
+++ Practica-TDD/Tests/FizzBuzzTest.java
@@ -48,4 +48,29 @@ class FizzBuzzTest {
         assertEquals("fizz", cocheRojo);
         System.out.println("Test con 6 ejecutado correctamente");
     }
+    void TestCon78Y9(){
+        //arrange (set up)
+        FizzBuzz prueba1 = new FizzBuzz();
+        FizzBuzz prueba2 = new FizzBuzz();
+        FizzBuzz prueba3 = new FizzBuzz();
+        //action
+        String cocheRojo = prueba1.calculo(7);
+        String cocheAmarillo = prueba2.calculo(8);
+        String cocheVerde = prueba3.calculo(9);
+        //assert
+        assertEquals("7", cocheRojo);
+        assertEquals("8", cocheAmarillo);
+        assertEquals("fizz", cocheVerde);
+        System.out.println("Test con 7, 8 y 9 ejecutado correctamente");
+    }
+    @Test
+    void TestCon10(){
+        //arrange (set up)
+        FizzBuzz prueba1 = new FizzBuzz();
+        //action
+        String cocheRojo = prueba1.calculo(10);
+        //assert
+        assertEquals("buzz", cocheRojo);
+        System.out.println("Test con 10 ejecutado correctamente");
+    }
 }
